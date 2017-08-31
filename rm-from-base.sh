aec6380e3b002192594fcdfddcb675de4dac9f62
xushiwei <xushiweizh@gmail.com>
2017-09-01 01:28:21 +0800

    rm-from-base.sh

---
+++ rm-from-base.sh
@@ -0,0 +1,34 @@
+cd $QBOXROOT/base/com/src
+rm -rf qbox.us/bufio \
+	qbox.us/ds \
+	qbox.us/errors \
+	qbox.us/osl \
+	qbox.us/path \
+	qbox.us/runner
+
+cd $QBOXROOT/base/qiniu/src
+rm -rf github.com/qiniu/bits \
+	github.com/qiniu/bufio \
+	github.com/qiniu/bytes \
+	github.com/qiniu/bytes.v7 \
+	github.com/qiniu/ctype \
+	github.com/qiniu/ctype.v7 \
+	github.com/qiniu/errors \
+	github.com/qiniu/errors.v7 \
+	github.com/qiniu/log.v1 \
+	github.com/qiniu/log.v7 \
+	github.com/qiniu/osl/cmdline.v1 \
+	github.com/qiniu/osl/cmdline.v7 \
+	github.com/qiniu/osl/ino \
+	github.com/qiniu/osl/log \
+	github.com/qiniu/osl/signal \
+	github.com/qiniu/osl/sync \
+	github.com/qiniu/osl/unixsock \
+	github.com/qiniu/osl/vm \
+	github.com/qiniu/reqid.v1 \
+	github.com/qiniu/ts \
+	github.com/qiniu/ts.v7 \
+	github.com/qiniu/xlog.v1 \
+	github.com/qiniu/xlog.v7
+
+rm -rf qiniupkg.com/x
